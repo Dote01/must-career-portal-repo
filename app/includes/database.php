7142bff87cd40fd8807db3d1f30a0c6bf3fdb24f
kefa samwel <kefasamm@gmail.com>
2025-07-18 13:44:12 +0300

    removed the database hosted then set it into the docker

--- app/includes/database.php
+++ app/includes/database.php
@@ -1,11 +1,11 @@
 <?php
-$host = "mysql";
-$username = "root";
-$password = "";
+$host = "db";
+$user = "root";
+$password = "root";
 $dbname = "must_career";
 
 
-$conn = new mysqli($host, $usernmame, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
